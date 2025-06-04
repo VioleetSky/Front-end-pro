7771d709f79c0bc3bffafb8f98602e8691f8b79b
Anna Desnenko <annadesnenko@gmail.com>
2025-06-04 20:15:24 +0300

    Add homework 14

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const numberFirst=+prompt('Enter a number:');
+const numberSecond=+prompt('Enter a number:');
+const numberThird=+prompt('Enter a number:');
+
+const result=(numberFirst+numberSecond+numberThird)/3;
+
+alert(`The result of the arithmetic mean of three numbers: ${result}`);
